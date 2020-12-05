3fbc265319feea10603dccabd21be2eb8ac100b4
Andrija Ricko <andrija116@gmail.com>
2020-12-05 14:37:39 +0100

    RecyclerView databinding working
    Took 15 minutes

--- app/src/main/java/com/ricko/belablok/adapters/CurrentGameRvAdapter.kt
+++ app/src/main/java/com/ricko/belablok/adapters/CurrentGameRvAdapter.kt
@@ -9,26 +9,11 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
 import com.ricko.belablok.R
+import com.ricko.belablok.databinding.ItemCurrentGameBinding
 import com.ricko.belablok.db.Game
 
-class CurrentGameRvAdapter : RecyclerView.Adapter<CurrentGameRvAdapter.MyVh>() {
-    inner class MyVh(itemView: View): RecyclerView.ViewHolder(itemView){
-        var tv1: TextView = itemView.findViewById<MaterialTextView>(R.id.textView555)
-        var tv2: TextView = itemView.findViewById<MaterialTextView>(R.id.textView6)
-    }
-//    class GameViewHolder private constructor(val binding: ItemCurrentGameBinding) : RecyclerView.ViewHolder(binding.root) {
-//        fun bind(game: Game) {
-//            binding.currentGameRecyclerVar = game
-//            binding.executePendingBindings()
-//        }
-//        companion object {
-//            fun from(parent: ViewGroup): GameViewHolder {
-//                val layoutInflater = LayoutInflater.from(parent.context)
-//                val binding = ItemCurrentGameBinding.inflate(layoutInflater, parent, false)
-//                return GameViewHolder(binding)
-//            }
-//        }
-//    }
+class CurrentGameRvAdapter : RecyclerView.Adapter<CurrentGameRvAdapter.GameViewHolder>() {
+    inner class GameViewHolder(val binding: ItemCurrentGameBinding) : RecyclerView.ViewHolder(binding.root)
 
     private val diffUtil = object : DiffUtil.ItemCallback<Game>() {
         override fun areItemsTheSame(oldItem: Game, newItem: Game) = oldItem.id == newItem.id
@@ -39,15 +24,13 @@ class CurrentGameRvAdapter : RecyclerView.Adapter<CurrentGameRvAdapter.MyVh>() {
 
     fun submitListOfGames(games: List<Game>) = differ.submitList(games)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyVh(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_current_game, parent, false)
-//        ItemCurrentGameBinding.inflate(LayoutInflater.from(parent.context), parent, false).root
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = GameViewHolder(
+        ItemCurrentGameBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
 
-    override fun onBindViewHolder(holder: MyVh, position: Int) {
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
         val item = differ.currentList[position]
-        holder.tv1.text = item.player1Score.toString()
-        holder.tv2.text = item.player2Score.toString()
+        holder.binding.currentGameRecyclerVar = item
     }
 
     override fun getItemCount() = differ.currentList.size
